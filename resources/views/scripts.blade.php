d3a34d0326acdf92da3f6e62348356bca6261a15
Emanuel Mutschlechner <emanuelmutschlechner@gmail.com>
2020-01-06 19:44:50 +0100

    Update leaflet

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,7 +1,7 @@
 @if ($enabled)
     {{--TODO: If overriding service via @map() then service is not working--}}
     @if ($service == 'osm' || $service == 'bing' || $service == 'mapquest')
-        <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin="" type="text/javascript"></script>
+        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin="" type="text/javascript"></script>
         {{-- TODO check if bing needs polyfill: https://github.com/digidem/leaflet-bing-layer--}}
     @endif
     <script src="{{ asset(mix('js/index.js', 'vendor/maps')) }}" type="text/javascript"></script>
--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,6 +1,6 @@
 @if ($enabled)
     @if ($service == 'osm' || $service == 'bing' || $service == 'mapquest')
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin="" type="text/css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" type="text/css">
     @endif
     @if ($service == 'mapquest')
         <link rel="stylesheet" href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest-core.css" type="text/css">
